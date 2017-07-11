f4ab6ed70719c3df8eb8b933c0f7f9d5fea60d7d
John Jansen <john.jansen@me.com>
2017-07-10 21:48:06 -0700

    tests for char and string

--- spec/clamp_spec.cr
+++ spec/clamp_spec.cr
@@ -3,7 +3,35 @@ require "./spec_helper"
 describe Clamp do
   # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
+  describe "Char" do
+    it "lower" do
+      'a'.clamp('b', 'c').should eq 'b'
+      'b'.clamp('b', 'c').should eq 'b'
+    end
+
+    it "upper" do
+      'd'.clamp('b', 'c').should eq 'c'
+      'c'.clamp('b', 'c').should eq 'c'
+    end
+
+    it "within" do
+      'b'.clamp('a', 'c').should eq 'b'
+    end
+  end
+
+  describe "String" do
+    it "lower" do
+      "a".clamp("b", "c").should eq "b"
+      "b".clamp("b", "c").should eq "b"
+    end
+
+    it "upper" do
+      "d".clamp("b", "c").should eq "c"
+      "c".clamp("b", "c").should eq "c"
+    end
+
+    it "within" do
+      "b".clamp("a", "c").should eq "b"
+    end
   end
 end
